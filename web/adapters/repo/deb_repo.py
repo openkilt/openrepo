dfe2ed96a1bafbb3713e94aa40bff9046c3be9c6
Matt Hill <matthill@gmail.com>
2022-11-15 23:45:54 -0500

    Add prereq install

--- web/adapters/repo/deb_repo.py
+++ web/adapters/repo/deb_repo.py
@@ -32,7 +32,8 @@ class DepRepoAdapter(BaseRepoAdapter):
         base_url = f'http://{settings.DOMAIN_NAME}/{self.repo_uid}'
         dest_gpg_path = f'/usr/share/keyrings/openrepo-{self.repo_uid}.gpg'
 
-        repo_address = f'curl {base_url}/public.gpg | gpg --dearmor -o {dest_gpg_path}\n'
+        repo_address = f'apt update && apt install -y curl gpg\n'
+        repo_address += f'curl {base_url}/public.gpg | gpg --dearmor -o {dest_gpg_path}\n'
         repo_address += f'echo "deb [arch={DEB_ARCH_NAME} signed-by={dest_gpg_path}] {base_url}/ stable main" > /etc/apt/sources.list.d/openrepo-{self.repo_uid}.list\n'
         repo_address += 'apt update'
         return repo_address
