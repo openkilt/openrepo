517228ec9380329c662d807edb9e040f5144b70d
Ben Tyger <ben.tyger@tygerclan.net>
2026-02-28 14:00:52 -0500

    Added CSRF_TRUSTED_ORIGIN

--- web/openrepo/settings.py
+++ web/openrepo/settings.py
@@ -59,6 +59,9 @@ else:
     # https://security.stackexchange.com/questions/45687/what-does-djangos-allowed-hosts-variable-actually-do
     ALLOWED_HOSTS = ['*']
 
+    # Added CSRF support through a reverse proxy with different hostname. https://docs.djangoproject.com/en/4.0/ref/settings/#std-setting-CSRF_TRUSTED_ORIGINS
+    CSRF_TRUSTED_ORIGINS = os.getenv(OPENREPO_CSRF_TRUSTED_ORIGINS,[])
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
